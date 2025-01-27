0190a680bb31de9a7d298b061ef57bc919485552
PhCwb <116783478+PhCwb@users.noreply.github.com>
2025-01-27 06:10:02 -0300

    Add files via upload

---
+++ ExercicioLoja/ExercicioLoja/Entities/ImportedProduct.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace ExercicioLoja.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return CustomsFee + Price;
+        }
+
+        public override string PriceTag() //importados
+        {
+            return $"{Name} {TotalPrice().ToString("C")} (Customs fee: {CustomsFee.ToString("C")})";
+        }
+
+
+
+    }
+}
---
+++ ExercicioLoja/ExercicioLoja/Entities/UsedProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioLoja.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct() { }
+        public UsedProduct(string name, double price, DateTime manufactureDate) : base (name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag() //usados
+        {
+            return $"{Name} (used) {Price.ToString("C")} (Manufacture date: {ManufactureDate.ToString("dd/MM/yyyy")}";
+        }
+    }
+}
